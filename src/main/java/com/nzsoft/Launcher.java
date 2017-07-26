858daae717094307949077d2ee1379151dcfe615
teo <teodor.parvanov@gmail.com>
2017-07-26 21:40:50 +0300

    Making MockRPIOperationsImpl more modular

--- src/main/java/com/nzsoft/Launcher.java
+++ src/main/java/com/nzsoft/Launcher.java
@@ -1,13 +1,5 @@
 package com.nzsoft;
 
-import static com.nzsoft.rpi.RPIOperations.LEDState.OFF;
-import static com.nzsoft.rpi.RPIOperations.LEDState.ON;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-import org.apache.commons.lang3.StringUtils;
-
 import com.nzsoft.rpi.MockRPIOperationsImpl;
 import com.nzsoft.rpi.RPIOperations;
 import com.nzsoft.tasks.MockTaskOperationsImpl;
@@ -16,55 +8,58 @@ import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
-public class Launcher {
+import static com.nzsoft.rpi.RPIOperations.LEDState.OFF;
+import static com.nzsoft.rpi.RPIOperations.LEDState.ON;
+import static java.lang.Thread.sleep;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+public final class Launcher {
 
     public static void main(String[] args) throws Exception {
         RPIOperations rpiOperations = new MockRPIOperationsImpl();
         TaskOperations taskOperations = new MockTaskOperationsImpl();
-        final ButtonListener listener = new ButtonListener();
-        rpiOperations.registerButtonListener(listener);
 
-        //a second thread randomly clicks the button.
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                listener.isClicked = !listener.isClicked;
-            }
-        };
-        Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(timerTask, 0, 5000);
+        ButtonListener button = new ButtonListener();
+        rpiOperations.registerButtonListener(button);
+
         while (true) {
             //if there is an existing task get it, otherwise sleep
             String taskName = taskOperations.getTask();
-            if (StringUtils.isNotBlank(taskName)) {
+            if (isNotBlank(taskName)) {
                 //turn on the LED
                 rpiOperations.changeLED(ON);
                 //wait for the button to be clicked
-                while (!listener.isClicked) {
+                while (!button.isClicked()) {
                     //sleep 100ms then check again the button state
-                    Thread.sleep(100);
+                    System.out.println("Processing task...");
+                    sleep(100);
                 }
                 //mark the task as completed
                 taskOperations.markTaskCompleted(taskName);
                 //turn off the LED
                 rpiOperations.changeLED(OFF);
                 //sleep
-                Thread.sleep(1000);
+                sleep(1000);
             } else {
                 System.out.println("No tasks available. Sleeping for 1sec");
-                Thread.sleep(1000);
+                sleep(1000);
             }
         }
     }
 
     static final class ButtonListener implements GpioPinListenerDigital {
 
-        boolean isClicked;
+        private boolean clicked;
+
         @Override
         public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
             System.out.println(" --> BUTTON PIN STATE: " + event.getPin() + " = "
                     + event.getState());
-            isClicked = event.getState() == PinState.HIGH;
+            clicked = event.getState() == PinState.HIGH;
+        }
+
+        public boolean isClicked() {
+            return clicked;
         }
     }
 }
--- src/main/java/com/nzsoft/rpi/MockRPIOperationsImpl.java
+++ src/main/java/com/nzsoft/rpi/MockRPIOperationsImpl.java
@@ -1,6 +1,11 @@
 package com.nzsoft.rpi;
 
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MockRPIOperationsImpl implements RPIOperations {
 
@@ -10,7 +15,37 @@ public class MockRPIOperationsImpl implements RPIOperations {
     }
 
     @Override
-    public void registerButtonListener(GpioPinListenerDigital listener) {
+    public void registerButtonListener(final GpioPinListenerDigital listener) {
         System.out.println("Registered button listener");
+
+        //a second thread randomly clicks the button.
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                press();
+                zzz();
+                release();
+            }
+
+            private void release() {
+                listener.handleGpioPinDigitalStateChangeEvent(new GpioPinDigitalStateChangeEvent(new Object(), null, PinState.LOW));
+            }
+
+            private void press() {
+                listener.handleGpioPinDigitalStateChangeEvent(new GpioPinDigitalStateChangeEvent(new Object(), null, PinState.HIGH));
+            }
+
+            private void zzz() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 5000);
     }
+
 }
