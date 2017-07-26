72d6f3808025c3a00d5088e15fa3e5c288a624d4
teo <teodor.parvanov@gmail.com>
2017-07-26 22:44:22 +0300

    RPI integration fixes

--- src/main/java/com/nzsoft/rpi/RPIOperationsImpl.java
+++ src/main/java/com/nzsoft/rpi/RPIOperationsImpl.java
@@ -5,18 +5,17 @@ import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class RPIOperationsImpl implements RPIOperations {
 
-    // TODO use the correct pin numbers
-    private static final Pin BUTTON_INPUT_PIN = RaspiPin.GPIO_18;
-    private static final Pin LED_OUTPUT_PIN = RaspiPin.GPIO_01;
+    private static final Pin BUTTON_INPUT_PIN = RaspiPin.GPIO_26;
+    private static final Pin LED_OUTPUT_PIN = RaspiPin.GPIO_18;
 
     private final GpioController gpio;
-    private final GpioPinAnalogInput button;
+    private final GpioPinDigitalInput button;
     private final GpioPinDigitalOutput led;
 
     public RPIOperationsImpl() {
         gpio = GpioFactory.getInstance();
-        button = gpio.provisionAnalogInputPin(BUTTON_INPUT_PIN);
-        led = gpio.provisionDigitalOutputPin(LED_OUTPUT_PIN, "MinionLED", PinState.LOW);
+        button = gpio.provisionDigitalInputPin(BUTTON_INPUT_PIN, PinPullResistance.PULL_DOWN);
+        led = gpio.provisionDigitalOutputPin(LED_OUTPUT_PIN, "MinionLED", PinState.HIGH);
         led.setShutdownOptions(true, PinState.LOW);
     }
 
@@ -25,9 +24,11 @@ public class RPIOperationsImpl implements RPIOperations {
         switch (state) {
             case ON:
                 led.high();
+                System.out.println("LED ON");
                 break;
             case OFF:
                 led.low();
+                System.out.println("LED OFF");
                 break;
         }
     }
