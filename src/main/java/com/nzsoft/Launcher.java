79dfaa2085c8dabe61e2cf03e54f4d20078456c3
tspasov <tspasov@vmware.com>
2017-07-26 14:11:27 +0300

    Adding mock implementation of taskOperations and adding random button clicker daemon

--- src/main/java/com/nzsoft/Launcher.java
+++ src/main/java/com/nzsoft/Launcher.java
@@ -1,33 +1,70 @@
 package com.nzsoft;
 
-import com.nzsoft.rpi.MockRPIOperationsImpl;
-import com.nzsoft.rpi.RPIOperations;
-import com.nzsoft.rpi.RPIOperationsImpl;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-
 import static com.nzsoft.rpi.RPIOperations.LEDState.OFF;
 import static com.nzsoft.rpi.RPIOperations.LEDState.ON;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.nzsoft.rpi.MockRPIOperationsImpl;
+import com.nzsoft.rpi.RPIOperations;
+import com.nzsoft.tasks.MockTaskOperationsImpl;
+import com.nzsoft.tasks.TaskOperations;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Launcher {
 
     public static void main(String[] args) throws Exception {
         RPIOperations rpiOperations = new MockRPIOperationsImpl();
-        rpiOperations.registerButtonListener(new ButtonListener());
+        TaskOperations taskOperations = new MockTaskOperationsImpl();
+        final ButtonListener listener = new ButtonListener();
+        rpiOperations.registerButtonListener(listener);
 
+        //a second thread randomly clicks the button.
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                listener.isClicked = !listener.isClicked;
+            }
+        };
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 5000);
         while (true) {
-            rpiOperations.changeLED(ON);
-            Thread.sleep(1000);
-            rpiOperations.changeLED(OFF);
+            //if there is an existing task get it, otherwise sleep
+            String taskName = taskOperations.getTask();
+            if (StringUtils.isNotBlank(taskName)) {
+                //turn on the LED
+                rpiOperations.changeLED(ON);
+                //wait for the button to be clicked
+                while (!listener.isClicked) {
+                    //sleep 100ms then check again the button state
+                    Thread.sleep(100);
+                }
+                //mark the task as completed
+                taskOperations.markTaskCompleted(taskName);
+                //turn off the LED
+                rpiOperations.changeLED(OFF);
+                //sleep
+                Thread.sleep(1000);
+            } else {
+                System.out.println("No tasks available. Sleeping for 1sec");
+                Thread.sleep(1000);
+            }
         }
     }
 
     static final class ButtonListener implements GpioPinListenerDigital {
 
+        boolean isClicked;
         @Override
         public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
             System.out.println(" --> BUTTON PIN STATE: " + event.getPin() + " = "
                     + event.getState());
+            isClicked = event.getState() == PinState.HIGH;
         }
     }
 }
---
+++ src/main/java/com/nzsoft/tasks/MockTaskOperationsImpl.java
@@ -0,0 +1,30 @@
+package com.nzsoft.tasks;
+
+import java.util.UUID;
+
+public class MockTaskOperationsImpl implements TaskOperations {
+
+    @Override
+    public String getTask() {
+        if (Math.random() < 0.3) {
+            //30% chance to have a task
+            String taskName = UUID.randomUUID().toString();
+            System.out.println("Returned task " + taskName);
+            return taskName;
+        }
+        return null;
+    }
+
+    @Override
+    public Priority getTaskPriority(String taskName) {
+        Priority taskPriority = Math.random() < 0.33 ? Priority.LOW : (Math.random() < 0.66 ? Priority.MEDIUM : Priority.HIGH);
+        System.out.println(String.format("Task %s has priority %s", taskName, taskPriority));
+        return taskPriority;
+    }
+
+    @Override
+    public void markTaskCompleted(String taskName) {
+        System.out.println(String.format("Marking task with name %s as completed.", taskName));
+    }
+
+}
